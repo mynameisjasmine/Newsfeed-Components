d3ed43c2503b0ab51361f53d473c0202e0803818
Jasmine <jasminecharles@hotmail.com>
2019-07-10 12:35:43 -0700

    created menu

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -2,6 +2,23 @@
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
   menu.classList.toggle('menu--open');
+/*<div class="menu">
+      <ul>
+        <li>Students</li>
+        <li>Faculty</li>
+        <li>What's New</li>
+        <li>Tech Trends</li>
+        <li>Music</li>
+        <li>Log Out</li>
+      </ul>
+    </div> */
+    const menu = document.createElement('div');
+    const students = document.createElement('li')
+    const faculty = document.createElement('li')
+    const whatsNew = document.createElement('li')
+    const techTrends = document.createElement('li')
+    const music = document.createElement('li')
+    const logOut = document.createElement('li')
 
 }
 
