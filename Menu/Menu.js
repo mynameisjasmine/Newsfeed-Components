8d7231153776b384b5baa3c46c8b5b20606e016c
Jasmine <jasminecharles@hotmail.com>
2019-07-10 13:08:35 -0700

    menu text

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,7 +1,11 @@
 
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
-  menu.classList.toggle('menu--open');
+  //menu.classList.toggle('menu--open');
+
+
+}
+
 /*<div class="menu">
       <ul>
         <li>Students</li>
@@ -12,19 +16,46 @@ const toggleMenu = () => {
         <li>Log Out</li>
       </ul>
     </div> */
-    const menu = document.createElement('div');
-    const students = document.createElement('li')
-    const faculty = document.createElement('li')
-    const whatsNew = document.createElement('li')
-    const techTrends = document.createElement('li')
-    const music = document.createElement('li')
-    const logOut = document.createElement('li')
 
-}
+    // Defined new elements
+    const menu = document.createElement('div');
+    const menuList = document.createElement('ul');
+    const students = document.createElement('li');
+    const faculty = document.createElement('li');
+    const whatsNew = document.createElement('li');
+    const techTrends = document.createElement('li');
+    const music = document.createElement('li');
+    const logOut = document.createElement('li');
+
+//Structure of elements
+menu.appendChild(menuList)
+menuList.appendChild(students)
+menuList.appendChild(faculty)
+menuList.appendChild(whatsNew)
+menuList.appendChild(techTrends)
+menuList.appendChild(music)
+menuList.appendChild(logOut)
+
+//Setting class names
+
+menu.classList.add('menu')
+console.log(menu);
+
+//Adding text
+
+
+     students.textContent = 'Students'
+     faculty.textContent = 'Faculty'
+     whatsNew.textContent = 'What\'s New'
+     techTrends.textContent = 'Tech Trends'
+     music.textContent = 'Music'
+     logOut.textContent = 'Log out'
+
+
 
 
 // Start Here: Create a reference to the ".menu" class
-const menu = document.querySelector('.menu');
+//const menu = document.querySelector('.menu');
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
