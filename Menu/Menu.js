7462435d6039b3db1eb684f737a47634fec07699
Jasmine <jasminecharles@hotmail.com>
2019-09-04 13:39:37 -0700

    updates

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,71 +1,27 @@
+/* This is the data we will be using, study it but don't change anything, yet. */
 
+let menuItems = [
+  'Students',
+  'Faculty',
+  "What's New",
+  'Tech Trends',
+  'Music',
+  'Log Out'
+];
 
-
-
-const menuMarkUp = () => {
-
-  /*<div class="menu">
-      <ul>
-        <li>Students</li>
-        <li>Faculty</li>
-        <li>What's New</li>
-        <li>Tech Trends</li>
-        <li>Music</li>
-        <li>Log Out</li>
-      </ul>
-    </div> */
-
-    // Defined new elements
-    const menu = document.createElement('div');
-    const menuList = document.createElement('ul');
-    const students = document.createElement('li');
-    const faculty = document.createElement('li');
-    const whatsNew = document.createElement('li');
-    const techTrends = document.createElement('li');
-    const music = document.createElement('li');
-    const logOut = document.createElement('li');
-
-//Structure of elements
-menuList.appendChild(students)
-menuList.appendChild(faculty)
-menuList.appendChild(whatsNew)
-menuList.appendChild(techTrends)
-menuList.appendChild(music)
-menuList.appendChild(logOut)
-
-//Setting class names
-
-menu.classList.add('menu')
-console.log(menu);
-
-//Adding text
-
-
-     students.textContent = 'Students'
-     faculty.textContent = 'Faculty'
-     whatsNew.textContent = 'What\'s New'
-     techTrends.textContent = 'Tech Trends'
-     music.textContent = 'Music'
-     logOut.textContent = 'Log out'
-
-  return menuList;
-
-    };
-
-
-const toggleMenu = () => {
-      // Toggle the "menu--open" class on your menu refence. 
-      menuTwo.classList.toggle('menu--open');
-      menuParent = document.querySelector('.menu')
-      menuParent.appendChild(menuMarkUp());
-      
-    
-    }
-// Start Here: Create a reference to the ".menu" class
-const menuTwo = document.querySelector('.menu');
-// create a reference to the ".menu-button" class
-const menuButton = document.querySelector('.menu-button');
-// Using your menuButton reference, add a click handler that calls toggleMenu
-menuButton.addEventListener('click', toggleMenu)
-
-
+/* 
+  Step 1: Write a function that will create a menu component as seen below:
+  <div class="menu">
+    <ul>
+      {each menu item as a list item}
+    </ul>
+  </div>
+  The function takes an array as its only argument.
+  Step 2: Inside this function, iterate over the array creating a list item <li> element for each item in the array. 
+  Add those items to the <ul>
+  Step 3: Using a DOM selector, select the menu button (the element with a class of 'menu-button') currently on the DOM.
+  Step 4: add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on the menu (your div with a 'menu' class).
+  Step 5: return the menu component.
+  Step 6: add the menu component to the DOM.
+  
+*/
